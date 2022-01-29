fd81a53f4db11398295bc821483f1b92482c404b
MihailCoscodan <mihail.coscodan@acconomy.com>
2022-01-29 10:37:16 +0200

    Add username and pasword inputs, login button

--- CarRentApp/CarRentApp/LoginForm.Designer.cs
+++ CarRentApp/CarRentApp/LoginForm.Designer.cs
@@ -30,6 +30,11 @@ namespace CarRentApp
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lUsername = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,11 +48,60 @@ namespace CarRentApp
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // lUsername
+            // 
+            this.lUsername.AutoSize = true;
+            this.lUsername.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lUsername.Location = new System.Drawing.Point(858, 87);
+            this.lUsername.Name = "lUsername";
+            this.lUsername.Size = new System.Drawing.Size(97, 24);
+            this.lUsername.TabIndex = 1;
+            this.lUsername.Text = "Username";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(801, 137);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(289, 29);
+            this.tbUsername.TabIndex = 2;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(801, 241);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(289, 29);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // lPassword
+            // 
+            this.lPassword.AutoSize = true;
+            this.lPassword.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lPassword.Location = new System.Drawing.Point(858, 191);
+            this.lPassword.Name = "lPassword";
+            this.lPassword.Size = new System.Drawing.Size(92, 24);
+            this.lPassword.TabIndex = 3;
+            this.lPassword.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnLogin.Location = new System.Drawing.Point(852, 305);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(174, 49);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1154, 631);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lUsername);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(6);
@@ -62,6 +116,11 @@ namespace CarRentApp
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lUsername;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lPassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
